2a3638e9ba3b07936eafa05485bce0afc8831da0
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 23:11:31 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- build.rs
+++ build.rs
@@ -1,11 +1,3 @@
-fn main() {
-    if !version_check::is_feature_flaggable().unwrap_or(false) {
-        println!("cargo:rustc-cfg=use_fallback");
-    }
-
-    if version_check::is_max_version("1.38.0").unwrap_or(false)
-        || !version_check::Channel::read().unwrap().is_stable()
-    {
-        println!("cargo:rustc-cfg=skip_ui_tests");
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6238a0ad4f1146fbf55112419609e6449986154cf7ded1b5fdc978b06f4413b3
+size 327
